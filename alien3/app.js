2d2e81bec1bb4287a7b3cd4e97105c933dfccf33
faisalfarooqit <160407449+faisalfarooqit@users.noreply.github.com>
2024-04-24 10:22:03 -0700

    Add files via upload

---
+++ alien3/app.js
@@ -0,0 +1,20 @@
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("'player earned 10 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earned 15 points");
+}
+else if (alien_color === "brown") {
+    console.log("player earned 300 points");
+}
+var alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("'player earned 10 points");
+}
+else if (alien_color === "yellow") {
+    console.log("player earned 15 points");
+}
+else if (alien_color === "brown") {
+    console.log("player earned 300 points");
+}
---
+++ alien3/app.ts
@@ -0,0 +1,23 @@
+let alien_color =   "green";
+
+if(alien_color=== `green`){
+    console.log("'player earned 10 points");
+}
+else if (alien_color=== `yellow`){
+console.log("player earned 15 points");
+}
+else if (alien_color===`brown`){
+    console.log("player earned 300 points");
+}
+
+let alien_color =   "yellow";
+
+if(alien_color=== `green`){
+    console.log("'player earned 10 points");
+}
+else if (alien_color=== `yellow`){
+console.log("player earned 15 points");
+}
+else if (alien_color===`brown`){
+    console.log("player earned 300 points");
+}
